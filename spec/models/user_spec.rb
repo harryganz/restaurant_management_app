5715fd9335ab55b81c14443345af99f1b42d18e0
Harry Ganz <harry.y.ganz@gmail.com>
2016-03-27 19:57:43 -0400

    Adds validation tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,4 +27,24 @@ RSpec.describe User, type: :model do
 
     expect(User.where(name: "Harry").first).to be_nil
   end
+
+  it "throws an validation error if user doesn't have a name" do
+    user = User.new(role: "employee", password_digest: "password")
+    expect{user.save}.to raise_error("name cannot be blank")
+  end
+
+  it "throws a validation error if user doesn't have a role" do
+    user = User.new(name: "user", password_digest: "password")
+    expect{user.save}.to raise_error("role cannot be blank")
+  end
+
+  it "throws a validation error if user has a role which is not employee or manager" do
+    user = User.new(role: "invalid", name: "user", password_digest: "password")
+    expect{user.save}.to raise_error("role must be employee or manager")
+  end
+
+  it "lists all the orders for a user" do
+    expect(harry.orders).to eq([])
+  end
+
 end
