14c97136ddeb401403689071088723d9abac6330
Harry Ganz <harry.y.ganz@gmail.com>
2016-03-27 19:35:27 -0400

    Adds spec for user models

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "creates a new user" do
+    harry = User.create!(name: "Harry",
+    password_digest: "password", role: "employee")
+
+    expect(User.where(name: "Harry")).to eq(harry)
+  end
+
+  it "edits an existing user" do
+    harry = User.where(name: "Harry").first
+
+    harry.role = "manager"
+
+    harry.save!
+
+    expect(User.where(name: "Harry").first.role).to eq("manager")
+  end
+
+  it "removes an existing user" do
+    harry = User.where(name: "Harry").first
+
+    harry.destroy!
+
+    expect(User.where(name: "Harry").first).to be_nil
+  end
+end
