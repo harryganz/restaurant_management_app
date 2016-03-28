35228cdb2e95670282646d79616a09fa1ac3a92e
Harry Ganz <harry.y.ganz@gmail.com>
2016-03-27 20:42:18 -0400

    Adds validation tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -20,4 +20,47 @@ RSpec.describe Item, type: :model do
   it "removes an existing item" do
     expect(item1.destroy).to be_truthy
   end
+
+  it "throws error if name missing" do
+    item = Item.new(
+    description: "Penne pasta with farfala pasta",
+    price: 12.88, category: "entree")
+
+    expect{item.save!}.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it "throws error if description missing" do
+    item = Item.new(name: "Penne Farfala",
+    price: 12.88, category: "entree")
+    expect{item.save!}.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it "throws error if category missing" do
+    item = Item.new(name: "Penne Farfala",
+    description: "Penne pasta with farfala pasta",
+    price: 12.88)
+    expect{item.save!}.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it "throws error if price missing" do
+    item = Item.new(name: "Penne Farfala",
+    description: "Penne pasta with farfala pasta",
+    category: "entree")
+    expect{item.save!}.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it "throws error if price not greater than 0" do
+    item = Item.new(name: "Penne Farfala",
+    description: "Penne pasta with farfala pasta",
+    price: -1, category: "entree")
+    expect{item.save!}.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it 'throws error if category no appetizer, entree, salad, soup, beverage, '\
+  'or dessert' do
+    item = Item.new(name: "Penne Farfala",
+    description: "Penne pasta with farfala pasta",
+    price: 12.88, category: "bees")
+    expect{item.save!}.to raise_error(ActiveRecord::RecordInvalid)
+  end
 end
