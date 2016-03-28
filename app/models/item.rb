a44e3fd5a17dae3185ad0d64673d02f8dc89edbf
Harry Ganz <harry.y.ganz@gmail.com>
2016-03-27 20:48:47 -0400

    Adds validations to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ActiveRecord::Base
+  #Associations
+  #Validations
+  validates :name, :description, :category, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :category, inclusion: {
+    in: %w(appetizer entree salad beverage dessert)
+  }
 end
