d7c9469e9f2e6080d405246c1a90df3a38ba47a6
Harry Ganz <harry.y.ganz@gmail.com>
2016-03-28 11:46:09 -0400

    Adds database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user1 = User.create!(name: "Harry", role: "manager", password: "test")
+user2 = User.create!(name: "John", role: "employee", password: "test")
+
+item1 = Item.create!(name: "Penne Vodka",
+  category: "entree",
+  description: "Penne in our homemade vodka sauce",
+  price: 11.00)
+item2 = Item.create!(name: "Jalapeno Poppers",
+  category: "appetizer",
+  description: "Seasoned beef baked in jalapeno peppers",
+  price: 8.00)
+
+order1 = Order.new(party_name: "Donner", party_size: 8,
+  table_number: 5, special: "No Peanuts", user_id: user2.id)
+order1.items << item1
+order1.items << item2
+
+order1.save!
