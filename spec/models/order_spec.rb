f2ba2a1eaad8977fb2928e23d947d10943447bed
Harry Ganz <harry.y.ganz@gmail.com>
2016-03-27 21:14:22 -0400

    Adds validation tests for order model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -17,4 +17,44 @@ RSpec.describe Order, type: :model do
   it "removes an existing order" do
     expect(order1.destroy!).to be_truthy
   end
+
+  it "throws error if party_name missing" do
+    order = user.orders.new(party_size: 5, table_number: 10)
+    expect{order.save!}.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it "throws an error if party_size missing" do
+      order = user.orders.new(party_name: "Jones", table_number: 10)
+      expect{order.save!}.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it "throws an error if table_number missing" do
+      order = user.orders.new(party_name: "Jones", party_size: 5)
+      expect{order.save!}.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it "throws an error if user_id missing" do
+      order = Order.new(party_name: "Jones", party_size: 5, table_number: 10)
+      expect{order.save!}.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it "throws an error if party_size is not greater than 0" do
+      order = user.orders.new(party_name: "Jones", party_size: 0, table_number: 10)
+      expect{order.save!}.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it "throws an error of table_number is not greater than 0" do
+      order = user.orders.new(party_name: "Jones", party_size: 5, table_number: 0)
+      expect{order.save!}.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it "throws an error if table_number is not an integer" do
+      order = user.orders.new(party_name: "Jones", party_size: 5, table_number: 1.5)
+      expect{order.save!}.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it "throws an error if party_size is not an integer" do
+    order = user.orders.new(party_name: "Jones", party_size: 5.5, table_number: 1)
+    expect{order.save!}.to raise_error(ActiveRecord::RecordInvalid)
+  end
 end
