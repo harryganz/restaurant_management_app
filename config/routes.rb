98ba5efc0817ce4a618984531e28f49c43472911
Harry Ganz <harry.y.ganz@gmail.com>
2016-03-28 10:47:29 -0400

    Creates user controller/routes
    * Create basic user routes sans index
    * Also makes a login route for users

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  resources :users, except: [:index]  do
+    collection do
+      get 'login'
+    end
+  end
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
