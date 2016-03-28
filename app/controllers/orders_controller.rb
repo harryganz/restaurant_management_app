1d12f70e5d6bf4d544eb6a7570a1a1c9c691ec7b
Harry Ganz <harry.y.ganz@gmail.com>
2016-03-28 13:50:12 -0400

    Creates table for incomplete orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,4 @@
-class OrderController < ApplicationController
+class OrdersController < ApplicationController
   def index
     @incomplete_orders = Order.is_incomplete.by_time
     @complete_orders = Order.is_complete.by_table
