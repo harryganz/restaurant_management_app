98835c5b01e165328b00fd339a0a56ea4aa9bd5f
Harry Ganz <harry.y.ganz@gmail.com>
2016-03-27 21:17:36 -0400

    Adds validations for order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,10 @@ class Order < ActiveRecord::Base
   # Associations
   belongs_to :user
   has_and_belongs_to_many :items
+
+  #Validations
+  validates :party_name, :party_size, :table_number, :user_id, presence: true
+  validates :party_size, :table_number, numericality: {
+    only_integer: true, greater_than: 0
+  }
 end
