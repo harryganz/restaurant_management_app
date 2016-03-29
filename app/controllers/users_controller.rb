f0046d362e48297e215c72a3e1e94e8ca018825f
Harry Ganz <harry.y.ganz@gmail.com>
2016-03-28 21:26:15 -0400

    Sets up edit/add/index actions for users
    * Removes index routes and action
    * Adds data for add/edit/index actions in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,27 +2,47 @@ class UsersController < ApplicationController
   def login
   end
 
-  def new
+  def index
+    @users = User.all.order(:name)
   end
 
-  def show
+  def new
+      @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      redirect_to new_user_path
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      redirect_to edit_user_path(params[:id])
+    end
   end
 
   def destroy
+    if User.find(params[:id]).destroy
+      redirect_to users_path
+    else
+      redirect_to edit_user_path(params[:id])
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :password)
+    params.require(:user).permit(:name, :password, :password_confirmation, :role)
   end
 end
